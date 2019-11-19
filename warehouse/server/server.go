c3c869a22f6744c72f5669fad89318b3d672bd31
Bishoy Youssef <byoussef@pivotal.io>
2019-11-19 10:16:57 -0500

    wip
    Signed-off-by: Bishoy Youssef <byoussef@pivotal.io>

--- warehouse/server/server.go
+++ warehouse/server/server.go
@@ -2,14 +2,18 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
 	"github.com/concourse/dutyfree/infoData"
 	"github.com/gorilla/mux"
-	"net/http"
-	"fmt"
 )
 
 type Server struct {
-	Port int
+	Port   int
 	Exited chan bool
 }
 
@@ -18,15 +22,69 @@ func (s *Server) Start() {
 	r := mux.NewRouter()
 	r.HandleFunc("/info", InfoHandler)
 
+	staticHandler := staticServer{staticPath: "build", indexPath: "index.html"}
+	r.PathPrefix("/").Handler(staticHandler)
+
 	s.Exited = make(chan bool)
 
 	go func() {
-		err := http.ListenAndServe(":9090", r)
+		srv := &http.Server{
+			Handler: router,
+			Addr:    "127.0.0.1:9090",
+			// Good practice: enforce timeouts for servers you create!
+			WriteTimeout: 15 * time.Second,
+			ReadTimeout:  15 * time.Second,
+		}
+
+		err := srv.ListenAndServe()
 		if err != nil {
 			fmt.Println("ERROR: ", err)
 			s.Exited <- true
 		}
 	}()
+}
+
+// staticServer implements the http.Handler interface, so we can use it
+// to respond to HTTP requests. The path to the static directory and
+// path to the index file within that static directory are used to
+// serve the SPA in the given static directory.
+type staticServer struct {
+	staticPath string
+	indexPath  string
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h staticServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
 
 func InfoHandler(w http.ResponseWriter, r *http.Request) {
