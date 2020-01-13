dd04689b33b3162176e28c5fa820836d7c0d9234
James Thomson <jthomson@pivotal.io>
2020-01-13 14:00:19 -0500

    web: sort cards by name
    - also refactor repeating divs for loading spinner
    - remove unused banner background image
    
    Signed-off-by: James Thomson <jthomson@pivotal.io>

--- web/src/ResourceList/View.elm
+++ web/src/ResourceList/View.elm
@@ -4,11 +4,16 @@ import Card.View as CardView exposing (view)
 import Common.Common exposing (ResourceType)
 import Common.Overrides as Overrides exposing (grid)
 import Element exposing (Element, fill, height, maximum, paddingXY, width, wrappedRow)
+import List exposing (sortBy)
 import ResourceList.ResourceList exposing (container)
 
 
 view : List ResourceType -> String -> Element msg
 view resourceList githubIconImg =
+    let
+        sortedRL =
+            sortBy .name resourceList
+    in
     wrappedRow
         ([ width (fill |> maximum container.maxWidth)
          , height fill
@@ -16,7 +21,7 @@ view resourceList githubIconImg =
          ]
             ++ Overrides.grid
         )
-        (List.map (viewCard githubIconImg) resourceList)
+        (List.map (viewCard githubIconImg) sortedRL)
 
 
 viewCard : String -> ResourceType -> Element msg
